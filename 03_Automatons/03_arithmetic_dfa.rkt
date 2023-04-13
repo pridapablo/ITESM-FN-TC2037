57a64e8258583ce226135aa99254cce5aee08ed1
Pablo Banzo <pridapablo@gmail.com>
2023-04-13 09:56:46 -0600

    change 03

---
+++ 03_Automatons/03_arithmetic_dfa.rkt
@@ -0,0 +1,129 @@
+#|
+Implementation of a Deterministic Finite Automaton (DFA)
+
+A function will receive the definition of a DFA and a string,
+and return whether the string belongs in the language
+
+A DFA is defined as a state machine, with 3 elements:
+- Transition function
+- Initial state
+- List of acceptable states
+
+The DFA in this file is used to identify valid arithmetic expressions
+
+Examples:
+> (evaluate-dfa (dfa delta-arithmetic 'start '(int float exp)) "-234.56")
+'(float exp)
+
+> (arithmetic-lexer "45.3 - +34 / none")
+'(var spa)
+
+Gilberto Echeverria
+2023-03-30
+|#
+
+#lang racket
+
+(require racket/trace)
+
+(provide (all-defined-out))
+
+; Declare the structure that describes a DFA
+(struct dfa (func initial accept))
+
+(define (arithmetic-lexer strng)
+  " Call the function to validate using a specific DFA "
+  (evaluate-dfa (dfa delta-arithmetic 'start '(int float exp var spa)) strng))
+
+(define (evaluate-dfa dfa-to-evaluate strng)
+  " This function will verify if a string is acceptable by a DFA "
+  (let loop
+    ; Convert the string into a list of characters
+    ([chars (string->list strng)]
+     ; Get the initial state of the DFA
+     [state (dfa-initial dfa-to-evaluate)])
+    (cond
+      ; When the list of chars if over, check if the final state is acceptable
+      [(empty? chars) (member state (dfa-accept dfa-to-evaluate))]
+      [else (loop (cdr chars)
+                  ; Apply the transition function to get the next state
+                  ((dfa-func dfa-to-evaluate) state (car chars)))])))
+
+
+(define (char-operator? char)
+  " Identify caracters that represent arithmetic operators "
+  (member char '(#\+ #\- #\* #\/ #\= #\^)))
+
+(define (delta-arithmetic state char)
+  " Transition function to validate numbers
+  Initial state: start
+  Accept states: int float exp "
+  (case state
+    ['start (cond
+              [(char-numeric? char) (values 'int #f)]
+              [(or (eq? char #\+) (eq? char #\-)) (values 'sign #f)]
+              [(char-alphabetic? char)(values 'var #f)]
+              [(eq? char #\_)(values 'var #f)]
+              [else (values 'inv #f )])]
+    ['sign (cond
+             [(char-numeric? char) (values 'int #f)]
+             [else 'inv #f])]
+    ['int (cond
+            [(char-numeric? char) (values 'int #f)]
+            [(eq? char #\.) (values 'dot #f)]
+            [(or (eq? char #\e) (eq? char #\E))  (values 'e #f)]
+            [(char-operator? char) (values 'op 'int)]
+            [(eq? char #\space) (values 'spa 'int)]
+            [else (values 'inv #f )])]
+    ['dot (cond
+            [(char-numeric? char)  (values 'float #f)]
+            [else (values 'inv #f )])]
+    ['float (cond
+              [(char-numeric? char) (values 'float #f)]
+              [(or (eq? char #\e) (eq? char #\E)) (values 'e #f)]
+              [(char-operator? char) (values 'op 'float )]
+              [(eq? char #\space) (values 'spa #f)]
+              [else (values 'inv #f )])]
+    ['e (cond
+          [(char-numeric? char) (values 'exp #f)]
+          [(or (eq? char #\+) (eq? char #\-)) (values 'e_sign #f)]
+          [else (values 'inv #f )])]
+    ['e_sign (cond
+               [(char-numeric? char) (values 'exp #f)]
+               [else (values 'inv #f )])]
+    ['exp (cond
+            [(char-numeric? char) (values 'exp #f)]
+            [(char-operator? char) (values 'op 'exp)]
+            [(eq? char #\space) (values 'spa 'exp)]
+            [else (values 'inv #f )])]
+    ['var (cond
+            [(char-alphabetic? char) (values 'var #f)]
+            [(char-numeric? char) (values 'var #f)]
+            [(eq? char #\_) (values 'var #f)]
+            [(char-operator? char) (values 'op 'var)]
+            [(eq? char #\space) (values 'spa 'var)]
+            [else (values 'inv #f )])]
+    ['op (cond
+           [(char-numeric? char)(values 'int 'op)]
+           [(or (eq? char #\+) (eq? char #\-))(values 'sign 'op)]
+           [(char-alphabetic? char)(values 'var 'op)]
+           [(eq? char #\_)(values 'var 'op)]
+           [(eq? char #\space)(values 'op_spa 'op)]
+           [else (values 'inv #f )])]
+    ['spa (cond
+            [(char-operator? char) (values 'op #f )]
+            [(eq? char #\space) (values 'spa #f )]
+            [else (values 'inv #f )])]
+    ['op_spa (cond
+               [(char-numeric? char) (values 'int #f )]
+               [(or (eq? char #\+) (eq? char #\-)) (values 'sign #f )]
+               [(char-alphabetic? char) (values 'var #f )]
+               [(eq? char #\_) (values 'var #f )]
+               [(eq? char #\space) (values 'op_spa #f )]
+               [else (values 'inv #f )])]
+    ; Add parentheses to detect a parenthesis (opened and closed) as a token
+    ; Parenthesis is another state
+    ; Consider "(" as a token and ")" as a token
+    ;  ['paren (cond ...
+    ))
+
---
+++ 03_Automatons/DFA_numbers.rkt
@@ -0,0 +1,86 @@
+#|
+    Implementation of a Deterministic Finite Automaton (DFA).
+
+    A function that will recieve the definition of a DFA and a string,
+    and will return True if the string is accepted by the DFA
+    (if it is in the language of the DFA), and False otherwise.
+
+    A DFA is defined as a state machine
+
+    Pablo Banzo Prida
+    2023-03-30
+|#
+
+#lang racket
+
+(require racket/trace)
+
+(provide (all-defined-out))
+
+; Declare the structure of a DFA
+(struct dfa (trans-func initial accept))
+
+; Define the function that evaluates a DFA
+(define (arithmetic-lexer a-dfa strng)
+  " Evaluates a DFA"
+  (let loop
+    ([chars (string->list strng)]
+     [state (dfa-initial a-dfa)])
+    (cond
+      ; Member returns #t if the state is in the list of accept states
+      ; else returns the rest of the list after that index
+      [(empty? chars) (member state (dfa-accept a-dfa))] ; If the final state is an accept state
+      [else (loop (rest chars)
+                  ; Apply the transition function to get the next state
+                  ((dfa-trans-func a-dfa) state (first chars)))])))
+
+; Transition function (use char-numeric?)
+(define (delta-numbers state char)
+  " Transition Function"
+  " Initial state: start"
+  " Accept states: int, float, exp"
+  (cond
+    ;' is a symbol, not a string
+    [(eq? state 'start) (cond
+                          [(char-numeric? char) 'int]
+                          [(eq? char #\+) 'sign]
+                          [(eq? char #\-) 'sign]
+                          [else 'inv])]
+    [(eq? state 'sign) (cond
+                         [(char-numeric? char) 'int]
+                         [else 'inv])]
+    [(eq? state 'int) (cond
+                        [(char-numeric? char) 'int]
+                        [(eq? char #\.) 'dot]
+                        [(eq? char #\e) 'e]
+                        [(eq? char #\E) 'e]
+                        [else 'inv])]
+    [(eq? state 'dot) (cond
+                        [(char-numeric? char) 'float]
+                        [else 'inv])]
+    [(eq? state 'float) (cond
+                          [(char-numeric? char) 'float]
+                          [(eq? char #\e) 'e]
+                          [(eq? char #\E) 'e]
+                          [else 'inv])]
+    [(eq? state 'e) (cond
+                      [(char-numeric? char) 'exp]
+                      [(eq? char #\+) 'e_sign]
+                      [(eq? char #\-) 'e_sign]
+                      [else 'inv])]
+    [(eq? state 'e_sign) (cond
+                           [(char-numeric? char) 'exp]
+                           [else 'inv])]
+    [(eq? state 'exp) (cond
+                        [(char-numeric? char) 'exp]
+                        [else 'inv])]
+    [else 'inv]))
+
+; An automaton is a list of states, a list of accept states, a transition function,
+; and an initial state
+; '(delta-numbers 'q0 '(q1))
+(define data (dfa delta-numbers 'start '(+)))
+(dfa-initial data)
+
+; Evaluate the following strings with the automaton
+(arithmetic-lexer data "67-23*temp")
