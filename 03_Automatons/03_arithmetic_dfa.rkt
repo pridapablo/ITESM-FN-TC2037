0598eca4bac3650adc760bfb39ef16266f0d7237
Pablo Banzo <pridapablo@gmail.com>
2023-04-21 22:32:42 -0600

    add error handling to arithmetic-lexer()

--- 03_Automatons/03_arithmetic_dfa.rkt
+++ 03_Automatons/03_arithmetic_dfa.rkt
@@ -34,7 +34,9 @@ Pablo Banzo Prida
 
 (define (arithmetic-lexer strng)
   " Call the function to validate using a specific DFA "
-  (evaluate-dfa (dfa delta-arithmetic 'start '(int float exp var spa paren comment)) strng))
+  (with-handlers ([exn? (lambda (exn) #f)]) ; Catch exceptions (#f means the string is not valid)
+    (evaluate-dfa (dfa delta-arithmetic 'start '(int float exp var spa paren comment)) strng)))
+
 
 (define (evaluate-dfa dfa-to-evaluate strng)
   " This function will verify if a string is acceptable by a DFA "
@@ -52,7 +54,7 @@ Pablo Banzo Prida
       [(empty? chars)
        (if (member state (dfa-accept dfa-to-evaluate))
            (reverse (cons (cons (list->string (reverse token-buffer)) state) tokens))
-           'inv)]
+           (values 'inv #f))] ; the final state is not acceptable
 
       [else
        (let-values
@@ -75,6 +77,7 @@ Pablo Banzo Prida
 
                  ; If not, add the current character to the token-buffer
                  [else (cons (car chars) token-buffer)])))])))
+
 
 (define (char-operator? char)
   " Identify caracters that represent arithmetic operators "
@@ -99,7 +102,7 @@ Pablo Banzo Prida
              [(eq? (char->integer char) 40) (values 'paren #f)]
              ;  [(eq? (char->integer char) 41) (values 'paren #f)]
              [(eq? char #\;) (values 'comment #f)]
-             [else 'inv #f])]
+             [else (values 'inv #f )])]
     ['int (cond
             [(char-numeric? char) (values 'int #f)]
             [(eq? char #\.) (values 'dot #f)]
@@ -108,7 +111,7 @@ Pablo Banzo Prida
             [(eq? char #\space) (values 'spa 'int)]
             [(eq? (char->integer char) 40) (values 'paren 'int)]
             [(eq? (char->integer char) 41) (values 'paren 'int)]
-            [(eq? char #\;) (values 'comment #f)]
+            [(eq? char #\;) (values 'comment 'int)]
             [else (values 'inv #f )])]
     ['dot (cond
             [(char-numeric? char)  (values 'float #f)]
@@ -120,7 +123,7 @@ Pablo Banzo Prida
               [(eq? char #\space) (values 'spa 'float)]
               [(eq? (char->integer char) 40) (values 'paren #f)]
               [(eq? (char->integer char) 41) (values 'paren #f)]
-              [(eq? char #\;) (values 'comment #f)]
+              [(eq? char #\;) (values 'comment 'float)]
               [else (values 'inv #f )])]
     ['e (cond
           [(char-numeric? char) (values 'exp #f)]
@@ -193,105 +196,7 @@ Pablo Banzo Prida
 ; Use the lexer to validate a string
 
 (arithmetic-lexer "(3 + 6)-2 ;Hola como estas \n1 + 2 * 3 + 4")
-;  ; Numerical types
-"___NUMERICAL TYPES___"
-"Single digit"
-(arithmetic-lexer "2") ; '(("2" int)))
-"Multi digit int"
-(arithmetic-lexer "261") ; '(("261" int))
-"Negative int"
-(arithmetic-lexer "-63") ; '(("-63" int))
-"Single float"
-(arithmetic-lexer "5.23") ; '(("5.23" float))
-"Negative float"
-(arithmetic-lexer "-5.23") ; '(("-5.23" float))
-"Incorrect float"
-; (arithmetic-lexer ".23") ; #f
-; (arithmetic-lexer "2.2.3") ; #f
-"Exponent int"
-(arithmetic-lexer "4e8") ; '(("4e8" exp))
-"Exponent float"
-(arithmetic-lexer "4.51e8") ; '(("4.51e8" exp))
-"Negative exponent float"
-(arithmetic-lexer "-4.51e8") ; '(("-4.51e8" exp))
 
-;  ; Variables
-"___VARIABLES___"
-"Single variable"
-(arithmetic-lexer "data") ; '(("data" var))
-(arithmetic-lexer "data34") ; '(("data34" var))
-"Incorrect variable"
-; (arithmetic-lexer "34data") ; #f
-"Binary operation ints"
-(arithmetic-lexer "2+1") ; '(("2" int) ("+" op) ("1" int))
-"Invalid expression"
-; (arithmetic-lexer "/1") ; #f
-; (arithmetic-lexer "6 + 4 *+ 1") ; #f
-"Float and int"
-(arithmetic-lexer "5.2+3") ; '(("5.2" float) ("+" op) ("3" int))
-"Binary operation floats"
-(arithmetic-lexer "5.2+3.7") ; '(("5.2" float) ("+" op) ("3.7" float))
-
-;  ; Operations with variables
-"___OPERATIONS WITH VARIABLES___"
-"Binary operation variables"
-(arithmetic-lexer "one+two") ; '(("one" var) ("+" op) ("two" var))
-"Mixed variables numbers"
-(arithmetic-lexer "one+two/45.2") ; '(("one" var) ("+" op) ("two" var) ("/" op) ("45.2" float))
-
-;  ; Spaces between operators
-"___SPACES BETWEEN OPERATORS___"
-"Binary operation with spaces"
-(arithmetic-lexer "2 + 1") ; '(("2" int) ("+" op) ("1" int))
-"Multiple operators with spaces"
-(arithmetic-lexer "6 = 2 + 1") ; '(("6" int) ("=" op) ("2" int) ("+" op) ("1" int))
-"Mixed variables numbers spaces"
-(arithmetic-lexer "one + two / 45.2") ; '(("one" var) ("+" op) ("two" var) ("/" op) ("45.2" float))
-"Multiple operators"
-(arithmetic-lexer "97 /6 = 2 + 1")
-; '(("97" int) ("/" op) ("6" int) ("=" op) ("2" int) ("+" op) ("1" int))
-"Multiple float operators with spaces"
-(arithmetic-lexer "7.4 ^3 = 2.0 * 1")
-; '(("7.4" float) ("^" op) ("3" int) ("=" op) ("2.0" float) ("*" op) ("1" int))
-
-;  ; Parentheses
-"___PARENTHESES___"
-"Open and close"
-(arithmetic-lexer "()") ; '(("(" par_open) (")" par_close))
-"Open space close"
-(arithmetic-lexer "( )") ; '(("(" par_open) (")" par_close))
-"Open int close"
-(arithmetic-lexer "(45)") ; '(("(" par_open) ("45" int) (")" par_close))
 "Open space int space close"
 ; (arithmetic-lexer "( 45 )") ; '(("(" par_open) ("45" int) (")" par_close))
-"Open expression close"
-(arithmetic-lexer "(4 + 5)") ; '(("(" par_open) ("4" int) ("+" op) ("5" int) (")" par_close))
-"Open expression close"
-(arithmetic-lexer "(4 + 5) * (6 - 3)")
-; '(("(" par_open) ("4" int) ("+" op) ("5" int) (")" par_close) ("*" op)
-; ("(" par_open) ("6" int) ("-" op) ("3" int) (")" par_close))
 
-;  ; Comments
-"___COMMENTS___"
-"Variable and comment"
-(arithmetic-lexer "3; this is all") ; '(("3" int) ("; this is all" comment))
-"Expression and comment"
-(arithmetic-lexer "3+5 ; this is all") ; '(("3" int) ("+" op) ("5" int) ("; this is all" comment))
-"Complete expression 1"
-(arithmetic-lexer "area = 3.1415 * raduis ^2 ; area of a circle")
-; '(("area" var) ("=" op) ("3.1415" float) ("*" op) ("raduis" var) ("^" op) ("2" int)
-; ("; area of a circle" comment))
-"Complete expression 2"
-(arithmetic-lexer "result = -34.6e10 * previous / 2.0 ; made up formula")
-; '(("result" var) ("=" op) ("-34.6e10" exp) ("*" op) ("previous" var) ("/" op)
-; ("2.0" float) ("; made up formula" comment))
-"Complete expression 3"
-(arithmetic-lexer "cel = (far - 32) * 5 / 9.0 ; temperature conversion") ; '(("cel" var) ("=" op)
-; ("(" par_open) ("far" var) ("-" op) ("32" int) (")" par_close)
-; ("*" op) ("5" int) ("/" op) ("9.0" float) ("; temperature conversion" comment))
-
-;  ; Extreme cases of spaces before or after the expression
-"Spaces before"
-; (arithmetic-lexer "  2 + 1") ; '(("2" int) ("+" op) ("1" int))
-"Spaces before and after"
-; (arithmetic-lexer "  2 + 1 ") ; '(("2" int) ("+" op) ("1" int))
