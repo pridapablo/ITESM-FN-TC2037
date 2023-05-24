36d48c3eb1ad0907cdb95955c9691478e25328c6
Pablo Banzo <pridapablo@gmail.com>
2023-05-23 20:41:04 -0600

    more highlights for other types

--- 06_FinalProject/example.py
+++ 06_FinalProject/example.py
@@ -1,3 +1,69 @@
-"Hello world"
-# This is a comment
-print("Hello world")
+# Variable names
+my_variable = 42
+another_variable = "Hello"
+
+# Extended strings (multiline strings)
+multiline_string = '''
+This is a multiline string.
+It can span across multiple lines.
+'''
+
+# Parameters
+def greet(name):
+    print("Hello,", name)
+
+greet("Alice")
+
+# Decorators
+@decorator_function
+def my_function():
+    print("Decorated function")
+
+# Dictionaries
+my_dict = {
+    'key': 'value1',
+    'key': 'value2'
+}
+
+# Escaped characters
+escaped_string = "This is a double quote: \""
+
+# Arithmetic operations
+a = 5 + 3
+b = 10 - 2
+c = 4 * 2
+d = 16 / 4
+
+# Comparison operators
+if a > b:
+    print("a is greater than b")
+
+if c < d:
+    print("c is less than d")
+
+# Logical operators
+if a > 0 and b > 0:
+    print("Both a and b are positive")
+
+if c > 0 or d > 0:
+    print("At least one of c or d is positive")
+
+# Control flow statements
+for i in range(5):
+    print(i)
+
+while a > 0:
+    print(a)
+    a -= 1
+
+# Class definition
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+person = Person("Alice", 25)
+person.greet()
--- 06_FinalProject/syntaxhighlighter.exs
+++ 06_FinalProject/syntaxhighlighter.exs
@@ -29,11 +29,29 @@ defmodule Syntaxhighlighter do
   end
 
   @doc """
-  Helper function to highlight a line of Python code.
+  Helper function to highlight a line of Python code using regex and output HTML.
+
+  Issues:
+  - Everything matches when it's inside a comment or a string. For example:
+      # 34"yeah" print("hello") should be highlighted as a comment, but it's not.
+  - / is matching escaped characters, so it is not an operator currently.
+  - keyword "class" is not implemented because it will match "class" in the html tag.
+  - Multiline strings don't seem to work.
+  Extra things to try to highlight in the syntax highlighter:
+  - Variable names (words that are not keywords, functions, methods, etc.)
+  - Parameters (words followed by a comma and between parenthesis. Consider a function with only one parameter)
+  - Decorators (words preceded by @)
+  - Dictionaries (words followed by a colon)
   """
   defp highlight_line(line) when is_binary(line) do
     # Strings
     line = Regex.replace(~r/("[^"]*")/, line, "<span class=\"string\">\\1</span>")
+    # Multiline strings
+    line = Regex.replace(~r/("""[^"]*""")/, line, "<span class=\"string\">\\1</span>")
+    line = Regex.replace(~r/('''[^']*''')/, line, "<span class=\"string\">\\1</span>")
+
+    # Numbers
+    line = Regex.replace(~r/(\d+)/, line, "<span class=\"number\">\\1</span>")
 
     # Comments
     line = Regex.replace(~r/#(.*)$/, line, "<span class=\"comment\">#\\1</span>")
@@ -41,10 +59,28 @@ defmodule Syntaxhighlighter do
     # Keywords
     line =
       Regex.replace(
-        ~r/(\b(and|as|assert|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|not|or|pass|print|raise|return|try|while|with|yield)\b)/,
+        ~r/\b(def|if|else|elif|for|while|in|return)\b/,
         line,
         "<span class=\"keyword\">\\1</span>"
       )
+
+    # Operators (except /)
+    line = Regex.replace(~r/(\+|\-|\*)/, line, "<span class=\"operator\">\\1</span>")
+
+    # Booleans
+    line = Regex.replace(~r/\b(True|False)\b/, line, "<span class=\"boolean\">\\1</span>")
+
+    # Functions (words followed by a parenthesis)
+    line = Regex.replace(~r/\b(\w+)\(/, line, "<span class=\"function\">\\1</span>(")
+
+    # Parenthesis
+    line = Regex.replace(~r/(\(|\))/, line, "<span class=\"parenthesis\">\\1</span>")
+
+    # Methods (words preceded by a dot)
+    line = Regex.replace(~r/\.(\w+)/, line, ".<span class=\"method\">\\1</span>")
+
+    # Decorators (words preceded by @)
+    line = Regex.replace(~r/@(\w+)/, line, "@<span class=\"decorator\">\\1</span>")
 
     line
   end
@@ -57,15 +93,25 @@ defmodule Syntaxhighlighter do
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Syntax Highlighter</title>
         <style>
             .string { color: green; }
             .comment { color: gray; }
             .keyword { color: blue; }
+            .number { color: purple; }
+            .operator { color: red; }
+            .boolean { color: orange; }
+            .function { color: brown; }
+            .parenthesis { color: brown; }
+            .method { color: brown; }
+            .decorator { color: grey; }
+
         </style>
     </head>
     <body>
-        #{highlighted_text}
+    <pre>
+    #{highlighted_text}
+    </pre>
     </body>
     </html>
     """
