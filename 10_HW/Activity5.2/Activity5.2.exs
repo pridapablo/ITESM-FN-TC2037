d0ce8d102c6567abdf579cfb90bd55b99ae7903f
Pablo Banzo <pridapablo@gmail.com>
2023-06-13 23:33:15 -0600

    comment code

--- 10_HW/Activity5.2/Activity5.2.exs
+++ 10_HW/Activity5.2/Activity5.2.exs
@@ -1,15 +1,18 @@
 defmodule Hw do
   # Helper function to check if a number is prime A number is prime if it is only divisible by 1 and
-  # itself
+  # itself (using pattern matching)
   defp is_prime?(n) when n < 2, do: false
   defp is_prime?(n), do: do_is_prime?(n, 2)
+  # n < divisor * divisor is the same as divisor > sqrt(n) (base case)
   defp do_is_prime?(n, divisor) when n < divisor * divisor, do: true
   defp do_is_prime?(n, divisor) when rem(n, divisor) == 0, do: false
   defp do_is_prime?(n, divisor), do: do_is_prime?(n, divisor + 1)
 
   # Helper function to sum all prime numbers in a given range (called by sum_primes_parallel)
   defp sum_primes_in_range({start, stop}) do
+    # Recieve a tuple of the start and stop numbers
     start..stop
+    # Filter out all non-prime numbers
     |> Enum.filter(&is_prime?/1)
     |> Enum.sum()
   end
@@ -21,8 +24,12 @@ defmodule Hw do
 
     0..(num_processes - 1)
     |> Enum.map(fn i ->
+      # The first two numbers are not prime, so we start at 2
       start = i * chunk_size + 2
+      # When we reach the last process, the stop number is the limit Otherwise, the stop number is
+      # the start number plus the chunk size
       stop = if i == num_processes - 1, do: limit, else: start + chunk_size - 1
+      # Return a tuple of the start and stop numbers
       {start, stop}
     end)
   end
@@ -56,7 +63,9 @@ end
 defmodule Timing do
   def time_execution(fun) do
     :timer.tc(fun)
+    # Get the time in microseconds
     |> elem(0)
+    # Convert to seconds
     |> Kernel./(1_000_000)
     |> IO.inspect(label: "Time in seconds")
   end
